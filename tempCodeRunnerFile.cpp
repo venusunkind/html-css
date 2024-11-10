e7b2ba8526726478d3cfa14d421ba5f5bfa08490
Aman Garg <garga1614@gmail.com>
2024-11-10 16:43:57 +0530

    Initial commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // short sa=9;
+    // printf("Aman is a good boy");
+    // cout<<"his age is "<<sa;
+    int marksinmaths=99;
+    cout<<marksinmaths<<endl;
+    printf("aman is not celebrating chhath puja");
+    return 0;
+}
